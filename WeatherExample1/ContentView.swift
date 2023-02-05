9068f87f194e5a049244a222b013c81f1e67c598
yigittekelioglu <yigittekelioglu@hotmail.com>
2023-02-06 00:45:43 +0300

    Initial Commit

---
+++ WeatherExample1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeatherExample1
+//
+//  Created by Yiğit Tekelioğlu on 6.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherExample1/WeatherExample1App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherExample1App.swift
+//  WeatherExample1
+//
+//  Created by Yiğit Tekelioğlu on 6.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherExample1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
